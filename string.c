56993cce5b9ce114f6a2f0a13916aa73a717c180
JANANICHANDRASEKAR <35103474+JANANICHANDRASEKAR@users.noreply.github.com>
2018-03-02 16:51:11 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	char a[10];
+	int i,n;
+	printf("enter the number");
+	scanf("%d",&n );
+	scanf("%s",&a);
+	for(i=0;i<=3;i++)
+	{
+                printf("%c",a[i]);
+	}
+}

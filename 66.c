d0766d88c7c968ab5326dc22b4dde0d6809b4542
JANANICHANDRASEKAR <35103474+JANANICHANDRASEKAR@users.noreply.github.com>
2018-03-02 16:19:46 +0530

    Update and rename 61.c to 66.c

---
+++ 66.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, flag = 0;
+    scanf("%d",&n);
+
+    for(i=2; i<=n; i++)
+    {
+        if(n%i==0)
+        {
+            flag=1;
+            break;
+        }
+    }
+
+    if (flag==1)
+        printf("not a prime");
+    else
+        printf( "a prime ");
+    
+    return 0;
+}

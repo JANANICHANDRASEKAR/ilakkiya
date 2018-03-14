72d6b3c3136791d5f855d8630ec8c2595c53cf83
JANANICHANDRASEKAR <35103474+JANANICHANDRASEKAR@users.noreply.github.com>
2018-03-14 16:03:20 +0530

    Update 62.c

--- 62.c
+++ 62.c
@@ -6,10 +6,10 @@ printf("enter the values ");
 scanf("%s",&a);
 if(a[i]=='0'||a[i]=='1')
 {
-printf("binary representation");
+printf("yes");
 }
 else
 {
-printf("not a binary representation");
+printf("no");
 }
 }
